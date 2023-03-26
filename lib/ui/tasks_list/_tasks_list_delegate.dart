15ee8da5a633db6971665486f54b9066d064334e
Jerinji2016 <jerinji2016@gmail.com>
2023-03-27 04:26:04 +0530

    show all created tasks

---
+++ lib/ui/tasks_list/_tasks_list_delegate.dart
@@ -0,0 +1,85 @@
+part of tasks_list;
+
+class _TasksListDelegate extends StatelessWidget {
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> data;
+
+  const _TasksListDelegate({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: data.length,
+      padding: const EdgeInsets.only(bottom: 32.0),
+      itemBuilder: (context, index) {
+        QueryDocumentSnapshot<Map<String, dynamic>> snapshot = data.elementAt(index);
+        Task task = Task.fromJson(snapshot.id, snapshot.data());
+        return _TaskTile(task: task);
+      },
+    );
+  }
+}
+
+class _TaskTile extends StatelessWidget {
+  final Task task;
+
+  const _TaskTile({
+    Key? key,
+    required this.task,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Material(
+        color: Colors.white,
+        elevation: 8.0,
+        shadowColor: Colors.grey[100],
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+        ),
+        child: InkWell(
+          onTap: () {},
+          borderRadius: const BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        task.name,
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 6.0),
+                      Text(
+                        task.description,
+                        style: const TextStyle(),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  globalDateFormat.format(task.dueDate),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/tasks_list/tasks_list.dart
+++ lib/ui/tasks_list/tasks_list.dart
@@ -1,18 +1,25 @@
 library tasks_list;
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:yodo/utils/globals.dart';
 
+import '../../modals/task.dart';
 import '../../routes.dart';
 import '../../widgets/primary_button.dart';
 import '../../widgets/profile_modal.dart';
 
 part '_no_tasks_view.dart';
+part '_tasks_list_delegate.dart';
 
 class TasksList extends StatelessWidget {
   const TasksList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    User user = FirebaseAuth.instance.currentUser!;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Yodo"),
@@ -37,7 +44,29 @@ class TasksList extends StatelessWidget {
           Icons.add,
         ),
       ),
-      body: const _NoTasksView(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("users")
+            .doc(user.uid)
+            .collection("tasks")
+            .orderBy("createdAt", descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            if (snapshot.data!.docs.isNotEmpty) {
+              return _TasksListDelegate(
+                data: snapshot.data!.docs,
+              );
+            } else {
+              return const _NoTasksView();
+            }
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
