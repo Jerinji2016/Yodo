f546c83e239cbd1db67326a2eb99d47c51a7057a
Jerinji2016 <jerinji2016@gmail.com>
2023-03-27 06:29:05 +0530

    refactor files

--- lib/ui/task_details/task_details.dart
+++ lib/ui/task_details/task_details.dart
@@ -10,6 +10,8 @@ import '../../utils/globals.dart';
 import '../../widgets/primary_bottom_sheet.dart';
 import '../task_editor/task_editor.dart';
 
+part '_task_action_buttons.dart';
+
 class TaskDetails extends StatelessWidget {
   static void showBottomModalSheet(BuildContext context, Task task) async {
     showModalBottomSheet(
@@ -150,59 +152,6 @@ class TaskDetails extends StatelessWidget {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class _TaskActionButtons extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  final VoidCallback onTap;
-  final Color color;
-
-  const _TaskActionButtons({
-    Key? key,
-    required this.icon,
-    required this.text,
-    required this.onTap,
-    this.color = Colors.orange,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      borderOnForeground: true,
-      elevation: 4.0,
-      shadowColor: Colors.grey[100]!,
-      color: color,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(8.0),
-        ),
-      ),
-      child: InkWell(
-        onTap: onTap,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(
-                icon,
-                color: Colors.white,
-              ),
-              const SizedBox(width: 4.0),
-              Text(
-                text,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
