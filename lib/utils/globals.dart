d25edc00eaeb09d70f46471a7aee890625e0ceac
Jerinji2016 <jerinji2016@gmail.com>
2023-03-29 22:51:02 +0530

    remove unused variable

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final DateFormat globalDateFormat = DateFormat("dd MMMM yyyy");
-
-const Color primaryColor = Color(0xFF480AC5);
 
 late final SharedPreferences preferences;
